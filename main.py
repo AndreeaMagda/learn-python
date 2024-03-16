e0450375321d54d12491ac2966fe91822a2ffb1b
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2024-03-16 17:28:59 +0200

    basics

---
+++ main.py
@@ -0,0 +1,30 @@
+# BASICS
+
+# Name Assignment
+# Variables & Constants
+
+my_first_variable=1
+my_first_variable=2
+
+print(type(my_first_variable))
+print(my_first_variable)
+
+my_first_variable="Now,I'm a string :)"
+print(type(my_first_variable))
+print(my_first_variable)
+
+# Functions
+
+def add_two_numbers(number1,number2):
+    total=number1+number2
+    print('Sum =',total)
+
+add_two_numbers(5,9)
+
+
+def add_two_numbers2(number_one, number_two):
+  result = number_one + number_two
+  return result
+
+print(add_two_numbers2(3,9))
+
